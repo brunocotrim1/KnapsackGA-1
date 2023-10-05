f20ceff781970a2516cfb1d40847a51467152852
brunocotrim1 <bruno.cotrim1@gmail.com>
2023-10-06 00:12:32 +0100

    Update analisys.py

--- analisys.py
+++ analisys.py
@@ -3,7 +3,33 @@ import numpy as np
 from scipy.stats import mannwhitneyu
 import matplotlib.pyplot as plt
 
+def statisticalAnalysis(compTuple1,compTuple2):
+  print("\n")
+  #tuple = (name,data)
+  # Assuming your CSV file has columns named 'data1' and 'data2', you can access these columns like this:
+  name1 = compTuple1[0]
+  name2 = compTuple1[0]
+  data1 = compTuple1[1]['time']
+  data2 = compTuple2[1]['time']
+  # Calculate mean and standard deviation
+  mean_data1 = np.mean(data1)
+  std_data1 = np.std(data1)
+  mean_data2 = np.mean(data2)
+  std_data2 = np.std(data2)
 
+  # Print the results
+  print(name1+': mean=%.3f stdv=%.3f' % (mean_data1, std_data1))
+  print(name2+': mean=%.3f stdv=%.3f' % (mean_data2, std_data2))
+  # compare samples
+  stat, p = mannwhitneyu(data1, data2)
+  print('Statistics=%.3f, p=%.3f' % (stat, p))
+  # interpret
+  alpha = 0.05
+  if p > alpha:
+      print('Same distribution (fail to reject H0) ')
+  else:
+      print('Different distribution (reject H0) '+name2 + ' has a relevant differente between distributions comparing to '+ name2)
+  print("\n")
 # Load data from CSV file
 filepaths = ['KnapsackGA.csv','KnapsackAlmostFullyParallel.csv','KnapsackFullyParallel.csv',
              'KnapsackFullyParallelSplitPop.csv','KnapsackGAPartiallyParallel.csv']
@@ -11,29 +37,11 @@ filepaths = ['KnapsackGA.csv','KnapsackAlmostFullyParallel.csv','KnapsackFullyPa
 
 dataframes = []
 for path in filepaths:
-  dataframes.append(pd.read_csv(path))
+  df = pd.read_csv(path)
+  dataframes.append(df)
 
-# Assuming your CSV file has columns named 'data1' and 'data2', you can access these columns like this:
-data1 = dataframes[0]['time']
-data2 = dataframes[1]['time']
-# Calculate mean and standard deviation
-mean_data1 = np.mean(data1)
-std_data1 = np.std(data1)
-mean_data2 = np.mean(data2)
-std_data2 = np.std(data2)
-
-# Print the results
-print('data1: mean=%.3f stdv=%.3f' % (mean_data1, std_data1))
-print('data2: mean=%.3f stdv=%.3f' % (mean_data2, std_data2))
-# compare samples
-stat, p = mannwhitneyu(data1, data2)
-print('Statistics=%.3f, p=%.3f' % (stat, p))
-# interpret
-alpha = 0.05
-if p > alpha:
-    print('Same distribution (fail to reject H0)')
-else:
-    print('Different distribution (reject H0)')
+for i in range(len(dataframes[1:])):
+   statisticalAnalysis((filepaths[0],dataframes[0]),(filepaths[i],dataframes[i]))
 
 
 import matplotlib.pyplot as plt
@@ -68,4 +76,4 @@ plt.subplots_adjust(left=0.3)  # Adjust this value as needed
 plt.savefig('boxplot.png', dpi=300, bbox_inches='tight')  # Adjust the file name and DPI as needed
 
 # Show the plot
-plt.show()
+#plt.show()
