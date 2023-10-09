46f1756cafd9555856b1821bcf80975ebe260f0a
brunocotrim1 <bruno.cotrim1@gmail.com>
2023-10-09 01:01:40 +0100

    asa

--- src/main/java/fcul/ppc/parallelization/ParallelFW.java
+++ src/main/java/fcul/ppc/parallelization/ParallelFW.java
@@ -3,7 +3,7 @@ package fcul.ppc.parallelization;
 public class ParallelFW {
     public static void doInParallel(Parallelizable pw, int iterations) {
         int nThreads = Math.min(iterations, Runtime.getRuntime().availableProcessors());
-        doInParallel(pw, iterations, nThreads/2);
+        doInParallel(pw, iterations, nThreads);
     }
 
     public static void doInParallel(Parallelizable pw, int iterations, int nThreads) {
